89fec5af0526e025e8ed1d8ef90c225bfa886bac
OHTA Shogo <athos0220@gmail.com>
2012-01-08 02:33:40 +0900

    GAEフレームワークを使うようにした

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
-print 'Content-Type: text/html'
-print ''
-print '''
-<html>
-<head><title>test</title></head>
-<body>
-  <h1>Hello, World!</h1>
-</body>
-</html>
-'''
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class HelloWorld(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write("Hello, World!")
+
+application = webapp.WSGIApplication([('/', HelloWorld)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
