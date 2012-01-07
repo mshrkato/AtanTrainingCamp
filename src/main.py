fee0cde716820b626d4e0892d57208ed0dde2894
OHTA Shogo <athos0220@gmail.com>
2012-01-08 01:54:07 +0900

    HTMLを吐くようにした

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
-print 'Content-Type: text/plain'
+print 'Content-Type: text/html'
 print ''
-print 'Hello, World!'
+print '''
+<html>
+<head><title>test</title></head>
+<body>
+  <h1>Hello, World!</h1>
+</body>
+</html>
+'''
