dbee4c683b83c447c613d3da22c92549d582f633
kinop <masataka.kinoshita@gmail.com>
2012-01-08 05:20:23 +0900

    post

--- src/handlers/bye.py
+++ src/handlers/bye.py
@@ -3,3 +3,12 @@ from google.appengine.ext import webapp
 class ByeWorld(webapp.RequestHandler):
     def get(self):
         self.response.out.write("Bye, World!!")
+        self.response.out.write('''
+            <form method="post" action="/bye/">
+                <textarea name="text"></textarea>
+                <input type="submit"></input>
+            </form>
+        ''')
+    def post(self):
+        text = self.request.get("text")
+        self.response.out.write(text)
