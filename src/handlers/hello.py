ca8ad73a6012c79a2a09a81a3fcfe0696d7eab9b
OHTA Shogo <athos0220@gmail.com>
2012-01-08 05:00:18 +0900

    GETパラメータを取得できるようにした

--- src/handlers/hello.py
+++ src/handlers/hello.py
@@ -2,4 +2,6 @@ from google.appengine.ext import webapp
 
 class HelloWorld(webapp.RequestHandler):
     def get(self):
-        self.response.out.write("Hello, World!!")
+        user = self.request.get('user')
+        age = self.request.get('age')
+        self.response.out.write("%s is %s years old." % (user, age))
