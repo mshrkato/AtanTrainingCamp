6f10a24102e80f27272610c06f683414cfbade26
OHTA Shogo <athos0220@gmail.com>
2012-01-22 10:42:35 +0900

    テンプレートを導入した

--- src/handlers/hello.py
+++ src/handlers/hello.py
@@ -1,7 +1,14 @@
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+from os.path import dirname, join
 
 class HelloWorld(webapp.RequestHandler):
     def get(self):
         user = self.request.get('user')
         age = self.request.get('age')
-        self.response.out.write("%s is %s years old." % (user, age))
+
+        values = {'user' : user, 'age' : age}
+        path = join(dirname(dirname(__file__)), 'template', 'hello.html')
+        body = template.render(path, values)
+        self.response.out.write(body)
